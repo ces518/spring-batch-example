8fe09865031ed00adbd6cca06feaac5a910ab053
ces518 <pupupee9@gmail.com>
2019-08-14 16:43:21 +0900

    Job 실패 유도 코드

--- src/main/java/me/june/bacth/job/SimpleJobConfiguration.java
+++ src/main/java/me/june/bacth/job/SimpleJobConfiguration.java
@@ -33,6 +33,7 @@ public class SimpleJobConfiguration {
     public Job simpleJob () {
         return jobBuilderFactory.get("simpleJob")
                 .start(simpleStep1(null))
+                .next(simpleStep2(null))
                 .build();
     }
 
@@ -41,7 +42,17 @@ public class SimpleJobConfiguration {
     public Step simpleStep1 (@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info(">>>>> This is Step1");
+                    throw new IllegalArgumentException("step1 에서 실패");
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2 (@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
                     log.info(">>>>> request Date = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
